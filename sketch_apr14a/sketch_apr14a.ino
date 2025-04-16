61670aff848d44cd04cbeacb5a78548157251fd7
CAO NHU Y <145558888+NYCser@users.noreply.github.com>
2025-04-16 18:17:45 +0700

    Update sketch_apr14a.ino

--- sketch_apr14a/sketch_apr14a.ino
+++ sketch_apr14a/sketch_apr14a.ino
@@ -1,10 +1,8 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <MFRC522.h
+#include <MFRC522.h>
 #define SS_PIN 5
 #define RST_PIN 0
-
-//Xuan Thuc
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
